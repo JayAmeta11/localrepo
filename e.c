d086c121fc8f41384c528620d3a5ff53471e07b2
JayAmeta11 <jayameta2006@gmail.com>
2024-09-29 01:59:05 +0530

    josephus problem

--- e.c
+++ e.c
@@ -1,6 +1,26 @@
 #include<stdio.h>
 int main(){
-    int a=4,b=3;
-    printf("%d",a+b);
-
+    int n;//number of people
+    printf("enter number of people : ");
+    scanf("%d",&n);
+int a[n];
+for(int i=0;i<n;i++){
+    a[i]=1;
+}
+for(int j = 0 ; j<n;j++){
+    for(int i = 0;i<n;i++){
+    if(a[i]!=0&&i+1+j<n){
+        a[i+1+j]=0;
+    }
+    else if(a[i]!=0&&i+1+j>=n){
+        a[n-i-1-j]=0;
+    }
+    }
+}
+for(int i=0;i<n;i++){
+    if(a[i]!=0){
+        printf("alive person is %d",i+1);
+    }
+}
+return 0;
 }
