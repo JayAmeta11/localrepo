1369848188fb741ebac32e6ca56a1a6d5dcd9230
JayAmeta11 <jayameta2006@gmail.com>
2024-09-29 10:19:06 +0530

    correct

--- e.c
+++ e.c
@@ -1,26 +1,20 @@
 #include<stdio.h>
+int josephus(int n,int k ){
+    int a; 
+    
+    if(n==1){
+        return 1;
+    }
+    a = (josephus(n-1,k)+k)%n;
+    return a;
+}
+
 int main(){
-    int n;//number of people
+    int n;
     printf("enter number of people : ");
     scanf("%d",&n);
-int a[n];
-for(int i=0;i<n;i++){
-    a[i]=1;
-}
-for(int j = 0 ; j<n;j++){
-    for(int i = 0;i<n;i++){
-    if(a[i]!=0&&i+1+j<n){
-        a[i+1+j]=0;
-    }
-    else if(a[i]!=0&&i+1+j>=n){
-        a[n-i-1-j]=0;
-    }
-    }
-}
-for(int i=0;i<n;i++){
-    if(a[i]!=0){
-        printf("alive person is %d",i+1);
-    }
-}
-return 0;
+    
+ int   sur = josephus(n,2) + 1;
+ printf("survivor is %d ",sur);
+    return 0;
 }
